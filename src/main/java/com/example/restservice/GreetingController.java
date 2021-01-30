3571252487435c7ddda43adb515cac270b3c2f65
amine521 <73544781+amine521@users.noreply.github.com>
2021-01-31 00:27:44 +0100

    Swagger

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -3,13 +3,11 @@ package com.example.restservice;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.example.restservice.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 //@RequestMapping(path = "/api")
 public class GreetingController {
 
